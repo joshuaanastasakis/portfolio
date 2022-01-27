15f5194bb01550460afcf177ee5b4d4523857087
Joshua Anastasakis <joshuaanastasakis@gmail.com>
2022-01-27 16:38:09 -0500

    Created Nav Toggle button for mobile view

--- javascript/index.js
+++ javascript/index.js
@@ -1,19 +1,25 @@
+const top_offset = 77;
+const visibility_threshold = 10;
+const nav_items = ["about", "experience", "projects", "resume", "contact"];
+const nav_items_reversed = nav_items.reverse();
+var cur_nav_item = "about";
+
 $(document).ready(function() {
     $('.go_to').click(function() {
         var data_id = $(this).data('id');
         console.log(data_id);
         
         $('html, body').animate({
-            scrollTop: $('#'+data_id).offset().top - 57 - 20 // 57="header_height", 20="margin"
+            scrollTop: $('#'+data_id).offset().top - top_offset // 57="header_height", 20="margin"
         },'slow');
-
-        $(this).blur();
 
     });
 
-    $(window).on('scroll', $.throttle(500, function() {
-        console.log("scroll");
-        // const 
+    $(window).on('scroll', $.throttle(100, function() {
+        const all_items = all_nav_items_in_view();
+        const item = all_items[0];
+        console.log(all_items);
+        console.log(" ");
     }));
 
     // toggle mobile navigation slide-over
@@ -23,9 +29,10 @@ $(document).ready(function() {
 
 
     // hide mobile nav slide-over on section selection
-    $('.nav_side').click(function() {
-        hide_nav();
-    });
+    // $('.nav_side').click(function() {
+    //     hide_nav();
+    // });
+    $('.nav_side').click(hide_nav);
 
 });
 
@@ -37,17 +44,80 @@ function hide_nav() {
 
 function show_nav() {
     $('.container_nav_top').animate({
-        "left": "00px"
+        "left": "0px"
     });
 }
 
-function inWindowView(el) {
-    var top_el = $(el).offset().top;
-    var bottom_el = $(el).offset().top - $(el).outerHeight();
-    var bottom_window = $(window).scrollTop() + $(window).innerHeight();
-    var top_window = $(window).scrollTop();
+function all_in_view(el) {
+    const offset = top_offset-1;
 
-    if ((bottom_window > bottom_el) && (top_window)) {
+    var e_top = Math.floor($(el).offset().top); // - top_offset);
+    var e_bot = Math.floor(e_top + $(el).outerHeight());
+    var w_top = Math.floor($(window).scrollTop() + offset);
+    var w_bot = Math.floor(w_top + $(window).innerHeight());
 
+    // console.log(" ");
+
+    console.log("window: " + w_top + " to " + w_bot);
+    // console.log(el + ": " + e_top + " to " + e_bot);
+    
+
+    var w_height = w_bot - w_top;
+    var e_height = e_bot - e_top;
+
+    if ((w_top <= e_bot && w_bot >= e_top) || (w_bot <= e_top && w_top >= e_bot)) {
+        var size_on_screen = 0;
+        var percent_on_screen = 0
+
+        if (w_top <= e_bot && w_bot >= e_top) {
+            // bottom part on screen
+            size_on_screen = e_bot - w_top;
+            percent_on_screen = size_on_screen / e_height;
+            console.log("BOTTOM PART - " + el + " (" + size_on_screen + ", " + (percent_on_screen).toFixed(2) + ")");
+        }
+        if (w_bot <= e_top && w_top >= e_bot) {
+            // top part on screen
+            size_on_screen = w_bot - e_top;
+            percent_on_screen = size_on_screen / e_height;
+            console.log("TOP PART - " + el + " (" + size_on_screen + ", " + (percent_on_screen).toFixed(2) + ")");
+        }
+        if (w_top <= e_top && e_bot <= w_bot) {
+            // all on screen
+            size_on_screen = e_height;
+            percent_on_screen = 1;
+            console.log("ALL - " + el + " (" + size_on_screen + ", " + (percent_on_screen).toFixed(2) + ")");
+        }
+        
+        console.log(el + " (" + size_on_screen + ", " + (percent_on_screen).toFixed(2) + ")");
+        return [el, percent_on_screen];
+    }
+    
+    return [null,0];
+}
+
+function all_nav_items_in_view() {
+    return nav_items.filter(i => {
+        return all_in_view("#"+i)[1] > 0;
+    });
+}
+
+function isIntersecting(e, w) {
+    // check top intersecting
+    if (e.top >= w.top) {
+        if (e.top > w.bottom) {
+            // off screen
+            return 0;
+        } else {
+            // on screen
+            
+            // check bottom intersecting
+            if (e.bot >= w.top) {
+                if (e.bot > w.bot) {
+                    // off screen
+                    return 0;
+                }
+                
+            }
+        }
     }
 }
